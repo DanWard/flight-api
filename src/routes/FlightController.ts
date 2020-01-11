3a1e77d970d93b0627938781b873fe5ced9e4269
Daniel ward <dan@deltaxent.com.au>
2020-01-11 14:31:27 +1100

    FlightController: wire up flight database

--- src/routes/FlightController.ts
+++ src/routes/FlightController.ts
@@ -1,14 +1,31 @@
 import { Router, Request, Response } from "express";
+import { FlightDatabase } from "src/database/FlightDatabase";
 
 export class FlightController {
     public router = Router();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    constructor(private database: any) {
+    constructor(private database: FlightDatabase) {
         this.router.get("/", this.getAll);
     }
 
-    getAll(req: Request, res: Response) {
-        res.send([]);
+    getAll = (req: Request, res: Response) => {
+        let results = [];
+
+        if (req.query.airline) {
+            results = this.database.find({
+                flightNumber: {
+                    "$contains": req.query.airline
+                }
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            } as any);
+        } else {
+            results = this.database.find({});
+        }
+
+        res.send({
+            flights: results,
+            size: results.length,
+            status: 200
+        });
     }
 }
