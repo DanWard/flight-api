286c0cbc4269cca54774bc996573702a1cfbba71
Daniel ward <dan@deltaxent.com.au>
2020-01-12 11:49:43 +1100

    Base: Expand comments and move lusca config

--- src/app.ts
+++ src/app.ts
@@ -13,19 +13,22 @@ import { FlightDatabase } from "./database/FlightDatabase";
 // Create Express server
 const app = express();
 
+// Get config required for this file
 const sessionConfig = config.get("session") as any;
 const expressConfig = config.get("express") as any;
+const luscaConfig = config.get("lusca") as any;
 
 if (expressConfig.useCompression) {
-    // Default config for compression
+    // Use defaults for compression
     app.use(compression());
 }
 
 if (expressConfig.useJsonParser) {
-    // Allow inbound POST requirements
+    // Allow inbound JSON POST requirements
     app.use(bodyParser.json());
 }
 if (expressConfig.useUrlEncParser) {
+    // Enable URL encoded POST bodys to be decoded
     app.use(bodyParser.urlencoded({ extended: true }));
 }
 
@@ -34,30 +37,29 @@ if (expressConfig.useSession) {
     app.use(session(sessionConfig));
 
     if (expressConfig.useLusca) {
-        app.use(lusca({
-            csrf: true,
-            xframe: "SAMEORIGIN",
-            p3p: "ABCDEF",
-            hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
-            xssProtection: true,
-            nosniff: true,
-            referrerPolicy: "same-origin"
-        }));
+        // Use lusca if sessions are enabled due to cookie/session requirements
+        app.use(lusca(luscaConfig));
     }
 
 }
 
-app.disable("x-powered-by");
 if (app.get("env") === "production") {
-    app.set("trust proxy", 1); // Allow single reverse proxy
+    // Allow single reverse proxy
+    app.set("trust proxy", 1);
 }
+
+// Disable the header that shows this server is running on node
+app.disable("x-powered-by");
 // END: Security hardening
 
 // Enable logging
 app.use(morgan("combined"));
 
+// BEGIN: Routes
 const flightDatabase = new FlightDatabase();
 const flightController = new FlightController(flightDatabase);
 app.use("/flights", flightController.router);
+// END: Routes
 
+// Export the running HTTP Server to enable tests to use it
 module.exports = app.listen(expressConfig.port, () => console.log(`[${new Date()}] Flight-API started on ${expressConfig.port}`));
