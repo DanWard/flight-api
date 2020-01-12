b60ec6f501bb4b6ff83f82294fd7eca720dcfa37
Daniel ward <dan@deltaxent.com.au>
2020-01-12 11:39:24 +1100

    Model: Expand comments

--- src/models/Flight.ts
+++ src/models/Flight.ts
@@ -2,14 +2,33 @@ import { Length, IsDate } from "class-validator";
 
 export class Flight {
 
+    /**
+     * Given flight number and IATA airline code, for example XYZ12.
+     */
     @Length(3, 8)
     public flightNumber!: string;
+
+    /**
+     * IATA location that the flight departed from.
+     */
     @Length(3)
     public departurePort!: string;
+
+    /**
+     * IATA location that the flight will arive in.
+     */
     @Length(3)
     public arrivalPort!: string;
+
+    /**
+     * Time of departure.
+     */
     @IsDate()
     public departureTime!: Date;
+
+    /**
+     * Time of arrival.
+     */
     @IsDate()
     public arrivalTime!: Date;
 
